8900e51b50457c850c949c743c3128a2dd2b23b8
T. Manikanta <t.manikanta006@gmail.com>
2025-05-26 14:30:58 +0530

    4th commit completed

--- script.js
+++ script.js
@@ -21,10 +21,15 @@ theme.addEventListener("click",( ) => {
     document.querySelector("#projects-section").style.color = "black";
     for(var j = 0; j < project.length;j++){
         project[j].style.backgroundColor = "white"
+        project[j].style.border = "black solid";
     }
     document.querySelector(".project-cont").style.backgroundColor = "rgba(255, 255, 255, 0.61)";
     document.querySelectorAll(".project-img")[0].setAttribute("src","icons/contact-mobile-svgrepo-com.svg");
     document.querySelectorAll(".project-img")[1].setAttribute("src","icons/person-fill-svgrepo-com.svg");
+    document.querySelector("#contact-section").style.color = "black";
+    document.querySelector(".contact-cont").style.boxShadow = "rgba(0, 0, 0, 0.74) 0px 5px 15px";
+    document.querySelector(".contact-cont").style.backgroundColor = "rgba(255, 255, 255, 0.61)";
+    
     
 })
 theme.addEventListener("dblclick",( ) => {
@@ -39,7 +44,7 @@ theme.addEventListener("dblclick",( ) => {
     document.querySelector("#skills-section").style.boxShadow = "rgba(255, 255, 255, 0.74) 0px 5px 15px";
     document.querySelector(".skill-cont").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
     for (let i = 0; i < skill.length; i++) {
-        skill[i].style.backgroundColor = "rgb(49, 49, 49)";        
+        skill[i].style.backgroundColor = "rgb(0, 0, 0)";        
     }
     document.querySelectorAll(".skill-icon")[9].setAttribute("src","icons/icons8-express-js-dark.svg");
     document.querySelectorAll(".skill-icon")[7].setAttribute("src","icons/icons8-nodejs-dark.svg");
@@ -47,9 +52,13 @@ theme.addEventListener("dblclick",( ) => {
     document.querySelectorAll(".skill-icon")[6].setAttribute("src","icons/react-logo-svgrepo-com-dark.svg");
     document.querySelector("#projects-section").style.color = "white";
     for(var j = 0; j < project.length;j++){
-        project[j].style.backgroundColor = "black"
+        project[j].style.backgroundColor = "black";
+        project[j].style.border = "white solid";
     }
     document.querySelector(".project-cont").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
     document.querySelectorAll(".project-img")[0].setAttribute("src","icons/contact-mobile-svgrepo-com-light.svg");
     document.querySelectorAll(".project-img")[1].setAttribute("src","icons/person-fill-svgrepo-com-light.svg");
+    document.querySelector("#contact-section").style.color = "white";
+    document.querySelector(".contact-cont").style.boxShadow = "rgba(255, 255, 255, 0.74) 0px 5px 15px";
+    document.querySelector(".contact-cont").style.backgroundColor = "rgba(0, 0, 0, 0.61)";
 })
